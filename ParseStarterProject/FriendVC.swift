a734bc2650cb74f88e7235f29a22f8041022ea17
Ricardo Bravo <ricardobravo@outlook.com>
2016-11-14 23:33:58 -0500

    wip(Menu): add items of menu with action to VC

---
+++ ParseStarterProject/FriendVC.swift
@@ -0,0 +1,44 @@
+//
+//  FriendVC.swift
+//  MiRed
+//
+//  Created by Ricardo Bravo Acuña on 14/11/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class FriendVC: UIViewController {
+
+    @IBOutlet var imgMenu: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        if self.revealViewController() != nil {
+            self.imgMenu.target = self.revealViewController()
+            self.imgMenu.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
